9677ec7bb4c402227977fba0eb7b837e86aebcbb
Arvinder Pal <arvinderpal10@gmail.com>
2024-06-03 23:46:28 +0530

    fmt -  Println in golang

---
+++ Packages/0001.fmtPackage/002.Println.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello World")
+	fmt.Println("This is printed on a new line.")
+}
+
+/*
+OutPut:
+Hello World
+This is printed on a new line.
+*/
